6473586d3754b0c9d53c189b628ec2cb845d9726
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-11-09 17:31:27 -0800

    new files

--- tensorflowJS/neural_network/sketch.js
+++ tensorflowJS/neural_network/sketch.js
@@ -4,7 +4,7 @@ const model = tf.sequential();
 // create the first hidden layer with 4 nodes taking in 3 inputs
 // Uses the sigmoid activation function
 const hidden = tf.layers.dense({
-  units: 4,
+  units: 3,
   inputShape: [3],
   activation: "tanh"
 });
@@ -32,50 +32,63 @@ model.compile({
 
 // training inputs
 const xs = tf.tensor2d([
-	[0,1,0], 
-	[0,1,1], 
-	[1,1,0],
-	[-1,1,0],
-	[-1,0,0],
-	[-1,0,1],
-	[1,0,1],
-	[0,1,0],
-	[0,0,0],
-	[-1,0,0],
-	[0,0,1],
-	[1,1,1],
-	[1,0,0],
-	[-1,1,1]]);
+  [0, 1, 0],
+  [0, 1, 1],
+  [1, 1, 0],
+  [-1, 1, 0],
+  [-1, 0, 0],
+  [-1, 0, 1],
+  [1, 0, 1],
+  [0, 1, 0],
+  [0, 0, 0],
+  [-1, 0, 0],
+  [0, 0, 1],
+  [1, 1, 1],
+  [1, 0, 0],
+  [-1, 1, 1]
+]);
 // training outputs
 const ys = tf.tensor2d([
-	[0],
-	[0],
-	[1],
-	[1],
-	[1],
-	[0],
-	[1],
-	[0],
-	[1],
-	[1],
-	[1],
-	[1],
-	[1],
-	[0]]);
+  [0],
+  [0],
+  [1],
+  [1],
+  [1],
+  [0],
+  [1],
+  [0],
+  [1],
+  [1],
+  [1],
+  [1],
+  [1],
+  [0]
+]);
 
 // make a prediction
 train().then(function() {
   console.log("training complete");
-  let testData = tf.tensor2d([-1,1,0])
+  let testData = tf.tensor2d([-1, 1, 0]);
   let y = model.predict(xs);
 
   y.print();
 });
 
 // train the model
+
 async function train() {
-  for (let i = 0; i < 10000; i++) {
-    const response = await model.fit(xs, ys);
-    console.log(i, response.history.loss[0]);
-  }
+  model.layers[0].setWeights([
+    [-2.2497578, -1.4616488, -0.2667132],
+    [1.9590461, -0.806393, 0.5483124],
+    [-0.1415037, -2.1341474, 0.4887933]
+  ]);
+
+  // for (let i = 0; i < 1000; i++) {
+  //   const response = await model.fit(xs, ys);
+
+  //   console.log(i, response.history.loss[0]);
+  // }
+
+  // console.log("Weights: ");
+  // model.layers[0].getWeights()[0].print();
 }
